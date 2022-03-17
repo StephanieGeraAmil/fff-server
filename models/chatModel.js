9b57c345eadeeb13a562900389f3f54ea21681b8
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-03-17 15:19:40 -0300

    renaming user model attribute to store private chats of the user and adding attributes of user to chats to model the case wjere the chats are private

--- models/chatModel.js
+++ models/chatModel.js
@@ -5,6 +5,8 @@ const chatSchema= mongoose.Schema(
         img:{type:String, default:''},
         date:{type:Date, default:new Date()},
         messages:  {type:Array, default:[]},
+        firstuser: {type:String},
+        seconduser:  {type:String},
        
      
     }
