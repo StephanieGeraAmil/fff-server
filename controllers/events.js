62299c6dca2504f97f39fd97034a7769f0cbb4da
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-03-31 18:00:40 -0300

    fixing update Event service

--- controllers/events.js
+++ controllers/events.js
@@ -1,6 +1,5 @@
 import EventModel from "../models/eventModel.js";
 import ChatModel from "../models/chatModel.js";
-import UserModel from "../models/userModel.js";
 import mongoose from 'mongoose';
 
 export const getEvents = async (req,res) =>{
@@ -18,30 +17,17 @@ export const getEventsOfUser=async (req,res)=>{
   
    try{       
          const chatsOfUser=  await ChatModel.find({"users":{ "$in": [_id]} });
-         console.log('chatsOfUser')
-         console.log(chatsOfUser)
-        
          const events=await EventModel.find();
-           let result={};
+         let result={};
          events.map(event=>{
-                   console.log('event chat')
-                  console.log(event.chat)
-                
-               if(JSON.stringify(chatsOfUser).includes(event.chat))
-               {
-           
-                   result= {...event._doc, hasTheUser:true};
-                 
+               if(JSON.stringify(chatsOfUser).includes(event.chat)){ 
+                  result= {...event._doc, hasTheUser:true};
                }else{
                    result= {...event._doc, hasTheUser:false};
                }
                eventsWithuserInfo.push(result);
-
          });
-          console.log(' eventsWithuserInfo')
-                  console.log(eventsWithuserInfo)
-
-         res.status(200).json(eventsWithuserInfo); 
+        res.status(200).json(eventsWithuserInfo); 
    }catch(error){
      res.status(404).json({message:error.message});
    }
@@ -70,28 +56,28 @@ export const updateEvent=async (req,res) =>{
    const {id:_id}=req.params;
    const updated=req.body;
    const task=updated.task;
-   const user=updated.user;
+   const user=updated.userToAddOrRemove;
    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({message:"invalid event id"});
    let updatedEvent={};
    
   try{
      if(task && user){
          if(!mongoose.Types.ObjectId.isValid(user)) return res.status(404).json({message:"invalid user id"});
-         const usr=await UserModel.findById(user);
          const event=await EventModel.findById(_id);
          const cht=await ChatModel.findById(event.chat);
-         const alreadyInEvent=cht.users.find(item=>item._id==user);  
-         if (task=='addUser'&&!alreadyInEvent){   
-            await ChatModel.findByIdAndUpdate(event.chat,{ $push: { users: usr } },{new:true}); 
+         if (task=='addUser'){   
+            await ChatModel.findByIdAndUpdate(event.chat,{ $push: { users: user } },{new:true}); 
          }else{
-            if(task=='deleteUser'&&alreadyInEvent){
-              await ChatModel.findByIdAndUpdate(event.chat,{ $pull: { users: usr } },{new:true});
+            if(task=='deleteUser'){
+              await ChatModel.findByIdAndUpdate(event.chat,{ $pull: { users: user } },{new:true});
             }
          }
      }else{
-         updatedEvent= await EventModel.findByIdAndUpdate(_id,updated,{new:true});    
+         await EventModel.findByIdAndUpdate(_id,updated, {new: true});    
      }
-      res.status(204).json(updatedEvent);  
+     const updated= await EventModel.findById(_id)
+     console.log(updated);
+      res.status(200).json(updated);  
    }catch(error){ 
       res.status(409).json({message:error.message});
    }
@@ -106,7 +92,7 @@ export const deleteEvent=async (req,res) =>{
       const eventToDelete= await EventModel.findById({ _id:_id });
       await ChatModel.deleteOne({ _id:eventToDelete.chat });
       const deleteEvent= await EventModel.deleteOne({ _id:_id });
-      res.status(204).json(deleteEvent);
+      res.status(200).json(deleteEvent);
    }catch(error){
       res.status(409).json({message:error.message});
    }       
